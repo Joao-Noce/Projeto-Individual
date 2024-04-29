bd0bcb7a245b9b3c4c11da7f7e267daba0c01336
Joao-Nocee <joao.noce@sptech.school>
2024-04-28 23:10:10 -0300

    Home e Assets

---
+++ Site_Home/Site_Home.js
@@ -0,0 +1,39 @@
+function showText1() {
+  document.getElementById("text1").style.left = "1px";
+  document.getElementById("text1").style.opacity = "80%";
+}
+
+function hideText1() {
+  document.getElementById("text1").style.left = "-250px";
+  document.getElementById("text1").style.opacity = "0%";
+}
+
+function showText2() {
+  document.getElementById("text2").style.left = "0";
+  document.getElementById("text2").style.opacity = "80%";
+}
+
+function hideText2() {
+  document.getElementById("text2").style.left = "-250px";
+  document.getElementById("text2").style.opacity = "0%";
+}
+
+function showText3() {
+  document.getElementById("text3").style.left = "0";
+  document.getElementById("text3").style.opacity = "80%";
+}
+
+function hideText3() {
+  document.getElementById("text3").style.left = "-250px";
+  document.getElementById("text3").style.opacity = "0%";
+}
+
+function showText4() {
+  document.getElementById("text4").style.left = "0";
+  document.getElementById("text4").style.opacity = "80%";
+}
+
+function hideText4() {
+  document.getElementById("text4").style.left = "-250px";
+  document.getElementById("text4").style.opacity = "0%";
+}
