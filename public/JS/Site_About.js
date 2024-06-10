017664a12150c06894af868e94ef485505ca4ee0
Joao-Nocee <joao.noce@sptech.school>
2024-06-10 00:50:42 -0300

    Alteração do vídeo na tela Sobre Mim, melhoria nas ferramentas e criação do arquivo js para a tela Sobre Mim

---
+++ public/JS/Site_About.js
@@ -0,0 +1,15 @@
+function video1_2() { // A
+    document.getElementById("caixa_pai").style.left = "-100vw";
+  }
+  
+  function video2_1() { // B
+    document.getElementById("caixa_pai").style.left = "0";
+  }
+  
+  function video2_3() { // C
+    document.getElementById("caixa_pai").style.left = "-200vw";
+  }
+  
+  function video3_2() { // D
+    document.getElementById("caixa_pai").style.left = "-100vw";
+  }
